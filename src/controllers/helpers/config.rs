4c3c58912d9dad3fb42796ab9222bb24e9d10907
MohanSharmav <catchmohansharma@gmail.com>
2023-08-29 13:37:37 +0900

    config_donne

--- src/controllers/helpers/config.rs
+++ src/controllers/helpers/config.rs
@@ -1,14 +1,12 @@
 use std::collections::HashMap;
 use config::Config;
-use serde::Deserializer;
 
 pub async fn db_config()-> Result<String,anyhow::Error>{
-    let base_path = std::env::current_dir()
-        .expect("Failed to determine the current directory");
+    let base_path = std::env::current_dir()?;
     let configuration_directory = base_path.join("configuration");
     let settings = Config::builder()
         .add_source(config::File::from(
-            configuration_directory.join("db_conf.toml"),
+            configuration_directory.join("db_configuration.toml"),
         )).build()?;
 
     let db_hashmap=settings.try_deserialize::<HashMap<String, String>>()?;
